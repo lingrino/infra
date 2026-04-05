6bdb0b06496ce3d5eda24f1785607062a7f7f1ad
Sean Lingren <sean@lingren.com>
2026-04-05 00:18:12 -0700

    somesecrets

---
+++ terraform/aws/accounts/prod/secrets_job.tf
@@ -0,0 +1,23 @@
+resource "aws_secretsmanager_secret" "apps_job_ci" {
+  name = "apps/job/ci"
+
+  tags = {
+    Name = "apps/job/ci"
+  }
+}
+
+data "aws_secretsmanager_secret_version" "apps_job_ci" {
+  secret_id = aws_secretsmanager_secret.apps_job_ci.id
+}
+
+resource "github_actions_secret" "apps_job_ci_oai_key" {
+  repository      = "job"
+  secret_name     = "OPENAI_API_KEY"
+  plaintext_value = jsondecode(data.aws_secretsmanager_secret_version.apps_job_ci.secret_string)["OPENAI_API_KEY"]
+}
+
+resource "github_actions_secret" "apps_job_ci_resend_key" {
+  repository      = "job"
+  secret_name     = "RESEND_API_KEY"
+  plaintext_value = jsondecode(data.aws_secretsmanager_secret_version.apps_job_ci.secret_string)["RESEND_API_KEY"]
+}
